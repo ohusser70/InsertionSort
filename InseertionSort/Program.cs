af16dd326dd3e256aa5cda787464b29f780868a7
ohusser70 <ohusser@gmail.com>
2017-09-27 21:23:51 +0200

    the project was renamed (typo)

--- InseertionSort/Program.cs
+++ InseertionSort/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace InseertionSort
+namespace InsertionSort
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ce sera un super projet de tri par insertion");
+            Console.ReadKey();
         }
     }
 }
